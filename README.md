ad586024dc5665860a10694d2fdac1bc88062dd2
Stephen Carr <stephen@mossend.software>
2026-02-08 12:19:15 +0000

    Yay

---
+++ README.md
@@ -0,0 +1,8 @@
+I want to build the following:
+
+  - an HTTP Server
+  - a REST API
+  - a Key Value Store
+  - A model system
+
+
---
+++ sleepy_api/mix.exs
@@ -0,0 +1,10 @@
+defmodule SleepyAPI.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sleepy_api,
+      version: "0.0.1"
+    ]
+  end
+end

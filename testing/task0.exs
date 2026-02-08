3b8e1e8f8d31d2e91e056196a38031daa06405fa
Stephen Carr <stephencarr906@gmail.com>
2026-02-08 21:26:05 +0000

    nice stuff

--- testing/task0.exs
+++ testing/task0.exs
@@ -1,11 +1,33 @@
 defmodule Foo do
-  def get_content_type(string) do
-    string
-    |> String.split(":")
-    |> Enum.at(1)
-    |> String.trim()
-    |> IO.puts()
-  end
-end
 
-Foo.get_content_type("content-type: application/json")
+  
+  def parse_http_file(file) do
+    case File.read(file) do
+      {:ok, contents} -> String.split(contents, "\n")
+      {:error, reason} -> reason
+    end
+  end
+
+  def find_content([line | rest], size) when size > 0 do
+    if String.contains?(line, "application/json") do
+      line
+    else
+      find_content(rest, size-1)
+    end
+  end
+
+  def find_content([], _size) do
+    :error
+  end
+
+  
+  def is_json?(string) do
+      string
+      |> String.split(":")
+      |> Enum.at(1)
+      |> String.trim()
+      |> String.contains?("application/json")
+  end
+
+
+
